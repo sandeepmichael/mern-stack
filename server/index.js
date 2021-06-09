e0af08b39c6fea73e102e548a08fb61c59027808
unknown <mahadarisandeep@gmail.com>
2021-06-09 22:51:13 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const app = express()
+const cors = require('cors')
+
+
+dotenv.config()
+//connect to DB
+mongoose.connect(process.env.MONGO_ID, {
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}, (err) => {
+    if(err) return console.error(err)
+    console.log("connected to mongoDB")
+})
+app.use(cors())
+app.use(express.json())
+
+
+//api routes
+app.use('/auth', require('./routes/userRoutes'))
+//...
+
+// setup build
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, (req, res) => {
+    console.log(`server is running on port ${PORT}`)
+})
